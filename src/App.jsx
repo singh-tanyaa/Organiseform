80a9e1dc02288af1f54da24f3de86ef0ae4f01d4
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-05-11 18:10:05 +0530

    initial commit
    formevent

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import OrganiseForm from "./components/OrganiseForm";
+
+// import { upValidation, eventValidation } from "./components/signupValidation";
+
+
+function App() {
+  return (
+    <>
+      <OrganiseForm />
+      
+      
+    </>
+  );
+}
+
+export default App;
